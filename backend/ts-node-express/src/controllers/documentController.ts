31582b530f0a9ba6b4042bdcff4187cc44137531
Aaron <aaronwan808@gmail.com>
2025-04-12 22:53:11 +0100

    created the baseline functions

--- backend/ts-node-express/src/controllers/documentController.ts
+++ backend/ts-node-express/src/controllers/documentController.ts
@@ -1,5 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 
-const uploadDocument = (req: Request, res: Response, next: NextFunction) => {
+export const uploadDocument = (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+export const deleteDocument = (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+export const updateDocument = (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+export const analyzeSingleDocument = (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+export const batchAnalyzeDocument = (req: Request, res: Response, next: NextFunction) => {
     
 }
+
