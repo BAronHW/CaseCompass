e04d1d311c9aaaf6142e382b1265364cb70490a3
Aaron <aaronwan808@gmail.com>
2025-05-18 18:39:56 +0100

    created vector db

---
+++ backend/ts-node-express/prisma/migrations/20250518172335_docuchunks/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "documentChunks" (
+    "id" SERIAL NOT NULL,
+    "embeddings" vector,
+    "documentId" INTEGER NOT NULL,
+
+    CONSTRAINT "documentChunks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "documentChunks_documentId_key" ON "documentChunks"("documentId");
+
+-- AddForeignKey
+ALTER TABLE "documentChunks" ADD CONSTRAINT "documentChunks_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
