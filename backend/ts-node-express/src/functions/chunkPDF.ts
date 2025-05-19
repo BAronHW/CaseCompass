cff45997f61877cb88e29e92f7a8639dd30af4e1
Aaron <aaronwan808@gmail.com>
2025-05-20 00:34:44 +0100

    chunkPDF function first implementation

--- backend/ts-node-express/src/functions/chunkPDF.ts
+++ backend/ts-node-express/src/functions/chunkPDF.ts
@@ -1,16 +1,44 @@
 /**
- * take a pdf buffer and
+ *  take a pdf buffer and write into a temporary file
+    try and load the file from the temp file path 
+    once loaded delete the tempfile path
  */
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { Blob } from "buffer";
+import { join } from "path";
+import { tmpdir } from "os";
+import { Document } from "@langchain/core/documents";
 
 export async function ChunkPDF(pdfBuffer: Buffer){
     try {
 
-        const blob = new Blob([pdfBuffer]);   
-        const loader = new PDFLoader(blob);
+        /
+
+        const tempFilePath = join(tmpdir(), `temp_pdf_${Date.now()}.pdf`);
+
+        const loader = new PDFLoader(tempFilePath, {
+            splitPages: true,
+        });
+
         const docs = await loader.load();
+
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 50,
+            chunkOverlap: 1,
+        });
+
+        const chunkedDocuments = await Promise.all(
+            docs.map((doc) => {
+                splitter.splitDocuments([
+                    new Document({
+                        pageContent: doc.pageContent,
+                        metadata: {...doc.metadata}
+                    })
+                ])
+            })
+        );
+
+        return chunkedDocuments.flat();
 
     }catch(error) {
         console.log('Error processing PDF Buffer: ', error);
