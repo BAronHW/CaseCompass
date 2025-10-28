6260a59da585b607efe3d534e2a18ff8b16e1f70
Aaron <aaronwan808@gmail.com>
2025-10-28 21:57:15 +0000

    trying to use npm install

---
+++ backend/ts-node-express/src/services/ContractAnalysisService.ts
@@ -0,0 +1,15 @@
+// 1. should be able to identify weaknesses in the contract
+export class ContractAnalysisService {
+
+    private ChunkContract() {
+        return;
+    }
+
+    private CreateContractAnalysisPrompt() {
+        return;
+    }
+
+    public AnalyzeContract() {
+        return;
+    }
+}
