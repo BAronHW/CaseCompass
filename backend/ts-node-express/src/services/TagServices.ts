6776a42e3b1b32f783397baf8640b5dfc51dfbf4
Aaron <aaronwan808@gmail.com>
2025-11-07 22:33:24 +0000

    adding tags

--- backend/ts-node-express/src/services/TagServices.ts
+++ backend/ts-node-express/src/services/TagServices.ts
@@ -1,4 +1,14 @@
+import { GoogleGenAI } from "@google/genai"
+import { db } from "../lib/prismaContext.js"
+import { Response } from "../models/models.js"
+
 class TagService {
+
+    private genAI: GoogleGenAI
+    
+    constructor(genAI: GoogleGenAI){
+        this.genAI = genAI
+    }
 
     public async GenerateTag() {
 
@@ -22,5 +32,52 @@ class TagService {
 
     public async GetAllTags() {
         
+    }
+
+    public async AttachTagToDoc(tagId: number, docId: number) {
+        const foundDoc = await db.document.findUnique({
+            where: {
+                id: docId
+            }
+        });
+
+        const foundTag = await db.tags.findUnique({
+            where: {
+                id: tagId
+            }
+        });
+
+        if (!foundDoc) {
+            const errorResponse = Response.createErrorResponse(
+                'Document not found',
+                404,
+                'DOCUMENT_NOT_FOUND'
+            );
+            throw errorResponse;
+        }
+
+        if (!foundTag) {
+            const errorResponse = Response.createErrorResponse(
+                'Tag not found',
+                404,
+                'TAG_NOT_FOUND'
+            );
+            throw errorResponse;
+        }
+
+        const updatedDoc = await db.document.update({
+            where: {
+                id: docId
+            },
+            data: {
+                tags: {
+                    connect: {
+                        id: tagId
+                    }
+                }
+            }
+        });
+
+        return updatedDoc;
     }
 }
