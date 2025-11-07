36a2a89667ab0424fde075f5872caaa5a6727f99
Aaron <aaronwan808@gmail.com>
2025-11-07 06:49:05 +0000

    starting tag service

---
+++ backend/ts-node-express/src/services/TagServices.ts
@@ -0,0 +1,26 @@
+class TagService {
+
+    public async GenerateTag() {
+
+    }
+
+    public async AddTag() {
+
+    }
+
+    public async EditTag() {
+
+    }
+
+    public async DeleteTag() {
+
+    }
+
+    public async GetTag() {
+
+    }
+
+    public async GetAllTags() {
+        
+    }
+}
